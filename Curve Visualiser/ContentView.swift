fd8ca62ab214de096568b19da575f320ad79ddf4
nilshoenson <nils@yummygum.com>
2021-12-20 22:06:33 +0100

    Initial Commit

---
+++ Curve Visualiser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Curve Visualiser
+//
+//  Created by Nils Hoenson on 20/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Curve Visualiser/Curve_VisualiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curve_VisualiserApp.swift
+//  Curve Visualiser
+//
+//  Created by Nils Hoenson on 20/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Curve_VisualiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
